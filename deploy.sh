0f175a46ceb6f74fbca3a4dff9f07eed78fd0cbf
Arpit Jain <arpit.jain@cohesity.com>
2023-10-14 15:45:40 +0530

    deployemnt script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+ng build --base-href="/angular-getting-started/"
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit --allow-empty -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/arpitjp/angular-getting-started.git main:gh-pages
+
+cd -
